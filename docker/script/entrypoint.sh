55b2e83d4b68245f564010a4b7f8dd3add23daf6
Long DO CAO <long.docao.partner@decathlon.com>
2021-10-18 12:02:58 +0200

    fix: fix fernet key

--- docker/script/entrypoint.sh
+++ docker/script/entrypoint.sh
@@ -4,7 +4,7 @@ TRY_LOOP="20"
 
 # Global defaults
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
+: "${AIRFLOW__CORE__FERNET_KEY:="JNTQOM2iPRvTT8Ar0wLfnqJirBJ3gKqZAwX7lQf3XOc="}"
 : "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
 
 AIRFLOW__CORE__LOAD_EXAMPLES=False
